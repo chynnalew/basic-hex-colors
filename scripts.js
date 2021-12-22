57efbe1d38c13ab9c89f98b42f2a1844233cda0a
chynnalew <chynnalew@yahoo.com>
2021-12-21 20:50:14 -0800

    add js to change background color in dom

--- scripts.js
+++ scripts.js
@@ -1,12 +1,31 @@
 function changeColor() {
-  const hexNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-  
-  let hexcode = '';
+  const hexNumbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
 
-  for (let i = 0; i <= 5; i++){
+  let hexcode = "";
+
+  for (let i = 0; i <= 5; i++) {
     let randomIndex = Math.floor(Math.random() * hexNumbers.length);
 
-    hexcode += hexNumbers[randomIndex]
+    hexcode += hexNumbers[randomIndex];
   }
 
+  document.getElementById("hex-code").innerHTML = hexcode;
+  document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
 }
